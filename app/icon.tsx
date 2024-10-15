328149fd2ab3696fe829c00fe172a0dbb72b2738
ritsvishalcs <ritsvishalcs@gmail.com>
2024-10-15 23:40:25 +0530

    first commit

---
+++ app/icon.tsx
@@ -0,0 +1,29 @@
+// import { ImageResponse } from "next/og";
+
+// export const runtime = "edge";
+// export const size = {
+//   width: 32,
+//   heigh: 32,
+// };
+// export const contentType = "Image/png";
+// export default function Icon() {
+//   return new ImageResponse(
+//     (
+//       <div
+//         style={{
+//           fontSize: 24,
+//           background: "black",
+//           width: "100%",
+//           height: "100%",
+//           display: "flex",
+//           justifyContent: "center",
+//           alignItems: "center",
+//           color: "white",
+//         }}
+//       >
+//         V
+//       </div>
+//     ),
+//     { ...size }
+//   );
+// }
